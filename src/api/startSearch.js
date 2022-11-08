73535c465a179f08c8f15d41c9a4d94acbd769d3
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-11-08 22:45:55 +0300

    start to do loading

--- src/api/startSearch.js
+++ src/api/startSearch.js
@@ -12,20 +12,10 @@ import axios from "axios";
 
 export async function startSearch(query, setSites) {
     // axios.defaults.headers['Access-Control-Allow-Origin'] = '*'
-    axios.get("http://localhost:8080/api/search", {
+    return await axios.get("http://localhost:8080/api/search", {
         params: {
             query
         }
     })
-        .then(function (response) {
-            const data = response.data.data
-            console.log(data)
-            setSites(data)
-        })
-        .catch(function (error) {
-            console.log(error);
-        })
-        .finally(function () {
-            //work anyway
-        });
+
 }
--- src/pages/search_page/search_page.jsx
+++ src/pages/search_page/search_page.jsx
@@ -6,6 +6,8 @@ import classes from './search_page.module.css'
 import MyInputContainer from "../../components/myInputContainer/myInputContainer";
 import ResultContainer from "../../components/resultContainer/resultContainer";
 import json from '../../json_templates/search.json'
+import {startSearch} from "../../api/startSearch";
+import {useFetching} from "../../hooks/useFetching";
 
 
 const SearchPage = () => {
@@ -14,6 +16,7 @@ const SearchPage = () => {
 
     const [result, setResult] = useState(false)
     const [sites, setSites] = useState([])
+
 
 
     useEffect( () => {
@@ -25,6 +28,11 @@ const SearchPage = () => {
         setSites(siteArray)
     }, [])
 
+    const [fetch, isLoading, isError] = useFetching(async () => {
+        const response = await startSearch(query, setSites)
+        console.log(response.data.data)
+    })
+
     return (
         <div className={classes.search__body}>
             <div className={classes.search__title}>
@@ -35,10 +43,9 @@ const SearchPage = () => {
                     <MySelector text='Все сайты'/>
                 </div>
                 <MyInputContainer query={query} setQuery={setQuery} setSites={setSites}/>
+
                 <ResultContainer sites={sites}/>
             </div>
-
-
         </div>);
 };
 

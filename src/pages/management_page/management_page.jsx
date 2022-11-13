97d4fe2f447a3f3e24c5c7f53e0ada3be351b5a8
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-11-13 22:00:29 +0300

    finish management page layout

--- src/pages/management_page/management_page.jsx
+++ src/pages/management_page/management_page.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './management_page.module.css'
 import MyButton from "../../components/myButton/myButton";
+import MyInput from "../../components/myInput/myInput";
 
 
 const ManagementPage = () => {
+    const [link, setLink] = useState('')
     return (
         <div className={classes.management__body}>
             <div className={classes.management__title}>
                 <h1>Управление</h1>
             </div>
-            <div className={classes.site_body}>
-                <MyButton children="Остановить индексацию"
 
-                />
+            <div className={classes.site_body}>
+                <div className={classes.button_container}>
+                    <MyButton children="Остановить индексацию"
+                    />
+                </div>
+                <div className={classes.input_container}>
+                    <div className={classes.input_title}>
+                        Добавить\Обновить ссылку
+                    </div>
+                    <div className={classes.input}>
+                        <MyInput placeholder="Ссылка"
+                                 value={link}
+                                 onChange={(e) => {setLink(e.target.value)}}
+                        />
+                    </div>
+                </div>
             </div>
 
         </div>

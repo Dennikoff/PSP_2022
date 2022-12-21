80b2f327cf9d1f92b4bce354bf545a0435f17aa5
denis0108 <71262767+denis0108@users.noreply.github.com>
2022-12-21 13:53:47 +0300

    add refresh token handler to all apis

--- src/api/updateLink.js
+++ src/api/updateLink.js
@@ -1,12 +1,26 @@
 import axios from "axios";
+import {storage} from "../storage/storage";
+import {refreshToken} from "../utils/refreshToken";
 
 export async function updateLink(url, isSelected) {
+    const tokens = storage.get('tokens')
     return await axios.post("http://localhost:8080/api/updateLink",null, {
+        headers: {
+            "Authorization": `Bearer ${tokens[0]}`
+        },
         params: {
             url,
             isSelected
         }
-    })
+    }).catch(
+        async (error) => {
+            if(error.response.status === 401) {
+                await refreshToken(updateLink)
+            } else {
+                console.log("error in response")
+            }
+        }
+    )
 }
 
 
--- src/api/updateLinks.js
+++ src/api/updateLinks.js
@@ -1,11 +1,26 @@
 import axios from "axios";
+import {storage} from "../storage/storage";
+import {refreshToken} from "../utils/refreshToken";
 
 export async function updateLinks(newMas) {
     console.log(newMas)
     let json = JSON.stringify(newMas)
+    const tokens = storage.get('tokens')
     return await axios.post("http://localhost:8080/api/updateLinks",  {
         data: json
-    })
+    },{
+        headers: {
+            "Authorization": `Bearer ${tokens[0]}`
+        }
+    }).catch(
+        async (error) => {
+            if(error.response.status === 401) {
+                await refreshToken(updateLinks)
+            } else {
+                console.log('error in response')
+            }
+        }
+    )
 
 }
 

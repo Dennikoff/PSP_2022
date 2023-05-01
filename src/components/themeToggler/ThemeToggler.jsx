7a0953c34a3ef9f3aeee0d69441390ba52457300
dharke <d.harke@yandex.ru>
2023-05-01 23:27:45 +0300

    change theme toggler and icons

--- src/components/themeToggler/ThemeToggler.jsx
+++ src/components/themeToggler/ThemeToggler.jsx
@@ -2,8 +2,10 @@ import React, {useContext, useEffect, useState} from 'react';
 import classes from './ThemeToggler.module.css'
 import sun from '../../img/sun.svg'
 import moon from '../../img/moon.svg'
-import { InputSwitch } from 'primereact/inputswitch';
+import {InputSwitch} from 'primereact/inputswitch';
 import {ThemeContext, themes} from "../../context/themContext";
+import {theme} from "antd";
+
 const ThemeToggle = () => {
 
     const [toggleValue, setToggleValue] = useState()
@@ -14,21 +16,33 @@ const ThemeToggle = () => {
 
     return (
         <div className={classes.toggleContainer}>
-            <span className={classes.toggleHeader}>Переключить тему</span>
-            <div className={classes.toggleContent}>
-                <img src={sun} alt=""/>
-                <InputSwitch
-                    checked={toggleValue}
-                    onChange={(e) => {
-                        setToggleValue(e.value)
-                        if (context.theme === themes.light) {
-                            context.changeTheme(themes.dark)
-                        }
-                        if (context.theme === themes.dark) {
-                            context.changeTheme(themes.light)
-                        }
-                    }}/>
-                <img src={moon} alt=""/>
+            <div className={classes.toggleContent} onClick={ () => {
+                if (context.theme === themes.light) {
+                    context.changeTheme(themes.dark)
+                }
+                if (context.theme === themes.dark) {
+                    context.changeTheme(themes.light)
+                }}}>
+                {
+                    context.theme === themes.dark
+                        ?
+                        <img src={sun} alt=""/>
+                        :
+                        <img src={moon} alt=""/>
+                }
+                {/*<img src={sun} alt=""/>*/}
+                {/*<InputSwitch*/}
+                {/*    checked={toggleValue}*/}
+                {/*    onChange={(e) => {*/}
+                {/*        setToggleValue(e.value)*/}
+                {/*        if (context.theme === themes.light) {*/}
+                {/*            context.changeTheme(themes.dark)*/}
+                {/*        }*/}
+                {/*        if (context.theme === themes.dark) {*/}
+                {/*            context.changeTheme(themes.light)*/}
+                {/*        }*/}
+                {/*    }}/>*/}
+                {/*<img src={moon} alt=""/>*/}
             </div>
         </div>
     );
